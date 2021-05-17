1fa0c6408afff2d1e4f8197642a1e75755e5f6c2
Dawood F.K Morris <22669106+DawoodMorris@users.noreply.github.com>
2021-05-17 11:50:55 +0200

    Added uppercase letters

--- functions.js
+++ functions.js
@@ -9,7 +9,7 @@ functions.createRandomString = (strLength) =>
     strLength = typeof strLength === 'number' && strLength > 0 ? strLength : false;
     if (strLength) 
     {
-        var possibleChars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+        var possibleChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
         var str = '';
         for (var i = 1; i <= strLength; i++) 
         {

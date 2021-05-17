84753394b53cd820d04e9673a8f254948acdc896
Dawood F.K Morris <22669106+DawoodMorris@users.noreply.github.com>
2021-05-17 11:44:20 +0200

    The first function

---
+++ functions.js
@@ -0,0 +1,27 @@
+//The functions container object
+const functions = {};
+/**
+ * Creates a randome string of length specified
+ * @param {*} strLength String outcome length: required
+ */
+functions.createRandomString = (strLength) =>
+{
+    strLength = typeof strLength === 'number' && strLength > 0 ? strLength : false;
+    if (strLength) 
+    {
+        var possibleChars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+        var str = '';
+        for (var i = 1; i <= strLength; i++) 
+        {
+            str += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
+        }
+        return str;
+    }
+    else 
+    {
+        return false;
+    }
+}
+
+
+module.exports = functions;
